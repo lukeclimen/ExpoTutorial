617131a098be480fc958c91916205d92288b299c
Luke Climenhage <luke.climenhage@gmail.com>
2024-10-08 21:02:34 -0400

    Replace root view with gesture handler

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
@@ -50,7 +51,7 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           placeholderImageSource={placeholderImage}
@@ -89,7 +90,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onClose={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
